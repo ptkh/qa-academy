ceadee9609675daf51cf512cae5126ced8ab378e
pkhachidze <p.khachidze@qa-academy.by>
2022-10-13 10:45:05 +0400

    Credentials file removed

--- tests/testData/test_data.py
+++ tests/testData/test_data.py
@@ -1,8 +1,7 @@
 class TestData:
-    email = "enmatheblack@gmail.com"
+    email =
     gmail_api_url = "https://gmail.googleapis.com"
     token_url = "https://www.googleapis.com/oauth2/v4/token"
-    refresh_token = \
-        "1//09n6daFu6p7cmCgYIARAAGAkSNgF-L9IrwpCLrLq0v0wzHQaIbgvhMpWaVrRbiv3mmqYHW_bpAew1icV_H3D7WVuN3UJOkPK0LQ"
-    client_id = "910826994971-hvkddr4eabigf6dj80359lnrmofo2f1a.apps.googleusercontent.com"
-    client_secret = "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"
+    refresh_token =
+    client_id =
+    client_secret =
